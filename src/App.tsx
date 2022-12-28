e87172bdf23d2b335160600273f7d8c245292a4f
alejandro waumann <awaumann@gmail.com>
2022-12-28 12:56:43 -0600

    Add app shell with header bar and content area

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import {
+  AppBar,
+  Box,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const App = () => {
 
   return (
-    <div>
-      quest board
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline enableColorScheme />
+      <AppBar position="relative" color="transparent" elevation={0}>
+        <Toolbar>
+          <Typography variant="h6">Quest Board</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ p: 4 }}>
+        <Container maxWidth="lg">
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 };
 
