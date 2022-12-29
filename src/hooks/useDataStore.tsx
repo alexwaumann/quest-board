ce84c32672ef700973743d48db042aab070c3e9c
alejandro waumann <awaumann@gmail.com>
2022-12-29 16:09:04 -0600

    Implement the datastore interface

--- src/hooks/useDataStore.tsx
+++ src/hooks/useDataStore.tsx
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import { getLocalStorage, setLocalStorage } from '../utils/localstorage';
 
 interface Goal {
   uid: string
@@ -41,11 +42,91 @@ interface DataStore {
   addMilestone: (goalUid: string, milestone: Milestone) => void
   addObjective: (objective: Objective) => void
 
-  completeGoal: (goalUid: string) => void
   completeMilestone: (goalUid: string, milestoneUid: string) => void
+  completeDaily: (objectiveUid: string, daily: Daily) => void
+
+  completeGoal: (goalUid: string) => void
   completeObjective: (objectiveUid: string) => void
-  completeDaily: (objectiveUid: string) => void
 };
 
-const useDataStore = create((set, get) => {
-});
+export const useDataStore = create<DataStore>((set, get) => ({
+  goals: getLocalStorage('goals', []),
+  objectives: getLocalStorage('objectives', []),
+
+  addGoal: (goal) => {
+    const newGoals = [...get().goals, goal];
+
+    set({ goals: newGoals });
+    setLocalStorage('goals', newGoals);
+  },
+
+  addMilestone: (goalUid, milestone) => {
+    const newGoals = get().goals.map((goal) => {
+      if(goal.uid === goalUid) {
+        goal.milestones.push(milestone);
+      }
+
+      return goal;
+    });
+
+    set({ goals: newGoals })
+    setLocalStorage('goals', newGoals);
+  },
+
+  addObjective: (objective) => {
+    const newObjectives = [...get().objectives, objective];
+
+    set({ objectives: newObjectives });
+    setLocalStorage('objectives', newObjectives);
+  },
+
+  completeMilestone: (goalUid, milestoneUid) => {
+    const newGoals = get().goals.map((goal) => {
+      if(goal.uid === goalUid) {
+        goal.milestones = goal.milestones.map((milestone) => {
+          if(milestone.uid === milestoneUid) {
+            milestone.completed = true;
+            milestone.date = TODAY();
+          }
+
+          return milestone;
+        });
+      }
+
+      return goal;
+    });
+
+    set({ goals: newGoals });
+    setLocalStorage('goals', newGoals);
+  },
+
+  completeDaily: (objectiveUid, daily) => {
+    const newObjectives = get().objectives.map((objective) => {
+      const isSelectedObjective = objective.uid === objectiveUid;
+      const isToday = daily.date === TODAY();
+      const isNew = objective.dailies.find((daily) => daily.date === TODAY()) === undefined;
+      if(isSelectedObjective && isToday && isNew) {
+        objective.dailies.push(daily);
+      }
+
+      return objective;
+    });
+
+    set({ objectives: newObjectives });
+    setLocalStorage('objectives', newObjectives);
+  },
+
+  completeGoal: (goalUid: string) => {
+    console.log('TODO: implement DataStore.completeGoal');
+  },
+
+  completeObjective: (objectiveUid) => {
+    console.log('TODO: implement DataStore.completeObjective');
+  },
+
+}));
+
+export const TODAY = (): number => {
+  const today = new Date();
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate()).getTime();
+};
